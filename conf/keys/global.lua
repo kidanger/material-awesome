de7127a0bdf7c9930e1b07b6dc63926a3f117ab7
Jérémy Anger <angerj.dev@gmail.com>
2019-02-16 21:02:59 +0100

    spotify shortcuts

--- conf/keys/global.lua
+++ conf/keys/global.lua
@@ -468,44 +468,21 @@ local globalKeys =
     --     end,
     --     {description = "volume 0%", group = "hotkeys"}),
 
-    -- MPD control
-    awful.key({ altkey, "Control" }, "Up",
+    awful.key({ modkey }, "Down",
         function ()
-            awful.spawn.with_shell("mpc toggle")
-            beautiful.mpd.update()
+            awful.spawn.with_shell 'dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause'
         end,
-        {description = "mpc toggle", group = "widgets"}),
-    awful.key({ altkey, "Control" }, "Down",
+        {description = "spotify toggle", group = "widgets"}),
+    awful.key({ modkey }, "Left",
         function ()
-            awful.spawn.with_shell("mpc stop")
-            beautiful.mpd.update()
+            awful.spawn.with_shell 'dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous'
         end,
-        {description = "mpc stop", group = "widgets"}),
-    awful.key({ altkey, "Control" }, "Left",
+        {description = "spotify toggle", group = "widgets"}),
+    awful.key({ modkey }, "Right",
         function ()
-            awful.spawn.with_shell("mpc prev")
-            beautiful.mpd.update()
+            awful.spawn.with_shell 'dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next'
         end,
-        {description = "mpc prev", group = "widgets"}),
-    awful.key({ altkey, "Control" }, "Right",
-        function ()
-            awful.spawn.with_shell("mpc next")
-            beautiful.mpd.update()
-        end,
-        {description = "mpc next", group = "widgets"}),
-    awful.key({ altkey }, "0",
-        function ()
-            local common = { text = "MPD widget ", position = "top_middle", timeout = 2 }
-            if beautiful.mpd.timer.started then
-                beautiful.mpd.timer:stop()
-                common.text = common.text .. lain.util.markup.bold("OFF")
-            else
-                beautiful.mpd.timer:start()
-                common.text = common.text .. lain.util.markup.bold("ON")
-            end
-            naughty.notify(common)
-        end,
-        {description = "mpc on/off", group = "widgets"}),
+        {description = "spotify toggle", group = "widgets"}),
 
     -- Copy primary to clipboard (terminals to gtk)
     awful.key({ modkey }, "c", function () awful.spawn("xsel -p -o | xsel -i -b") end,
